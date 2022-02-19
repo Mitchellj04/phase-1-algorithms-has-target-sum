4159b20ac93ea882078774e0273a864121b33bd1
JustinMitchell <mitchelljm04@gmail.com>
2022-02-19 16:32:53 -0500

    Assignment Completed

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+function hasTargetSum(array,target){
+const container = {};
+for (const numArray of array ){
+  const compliment = target - numArray;
+
+  if (container[compliment]) return true;
+
+  container[numArray] = true; 
 }
 
+ return false 
+}
+
+
+
+// function hasTargetSum(array, target) {
+//   for (let i = 0; i < array.length; i++){
+//     const container = target-array[i];
+//     for (let j = i + 1; j < array.length; j++){
+//       if (array[j] === container) {
+//         return true; 
+//       }
+//       else {
+//         return false;
+//       }
+//     }
+//   }
+// }
+console.log(hasTargetSum([7,2,4,6,8],12));
+// console.log(hasTargetSum([1,2,4,6],25));
+
+// Write your algorithm here
+/*
+1. write a function has TargetSum that takes two array
+2. The two array are array and Target
+3. The array should have a set of numbers
+4. if any two numbers in the array equal target array return true 
+
+*/
 /* 
   Write the Big O time complexity of your function here
 */
